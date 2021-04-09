3efba5b5bbbd97586ccc9da3cb5a377b7faf6f54
ClintFix <clint.fix@gmail.com>
2021-04-09 08:21:32 -0600

    get all actions router complete

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,27 @@
-// Write your "actions" router here!
+const express = require('express');
+const router = express.Router();
+
+const Actions = require('./actions-model');
+
+const {validateActionId} = require('../middleware/middleware')
+
+// [GET] - '/' - Get array of all actions 
+router.get('/', (req, res, next) => {
+    Actions.get()
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(err => {
+            next(err)
+        })
+})
+
+//Error Middleware
+router.use((err, req, res, next) => {
+    res.status(500).json({
+      message: "There has been an error",
+      error: err.message
+    });
+  })
+
+  module.exports = router;
