9a1b16f23397956af6f746e7485d0b386118304d
ClintFix <clint.fix@gmail.com>
2021-04-09 08:00:40 -0600

    server.js file setup

--- api/server.js
+++ api/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+
+const actionsRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router')
+
 const server = express();
 
-// Complete your server here!
-// Do NOT `server.listen()` inside this file!
+//Global Middleware
+server.use(express.json());
+
+//Routers
+server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectsRouter);
+
+server.get('/', (_,res) => {
+    res.send(`<h2>Clint's Sprint Challenge API</h2>`)
+})
 
 module.exports = server;
